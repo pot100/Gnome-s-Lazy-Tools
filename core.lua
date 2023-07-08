33ee64aaf87b0f5a3b54b55ac3f11da099a82dbc
pot100 <126993705+pot100@users.noreply.github.com>
2023-07-08 09:15:04 +0330

    Update core.lua

--- core.lua
+++ core.lua
@@ -1,45 +1,36 @@
 -- Addon Initialization
 local addonName, addonTable = ...
 local AceAddon = LibStub("AceAddon-3.0")
-local MyAddon = AceAddon:NewAddon(addonTable, addonName, "AceEvent-3.0")
-
--- Addon Variables
+local GnomesLazyTools = AceAddon:NewAddon(addonTable, addonName, "AceEvent-3.0")
+ -- Addon Variables
 local version = "<1.0.0>"
-
--- Addon Initialization Function
-function MyAddon:OnInitialize()
+ -- Addon Initialization Function
+function GnomesLazyTools:OnInitialize()
     -- Perform initialization tasks here
 end
-
--- Addon Enable Function
-function MyAddon:OnEnable()
+ -- Addon Enable Function
+function GnomesLazyTools:OnEnable()
     -- Register events and set up functionality here
 end
-
--- Addon Disable Function
-function MyAddon:OnDisable()
+ -- Addon Disable Function
+function GnomesLazyTools:OnDisable()
     -- Clean up and unregister events here
 end
-
--- Addon Functionality
-function MyAddon:MaxCameraZoom()
+ -- Addon Functionality
+function GnomesLazyTools:MaxCameraZoom()
     -- Implement code for controlling maximum camera zoom here
 end
-
-function MyAddon:ChangeWorldMapSize()
+ function GnomesLazyTools:ChangeWorldMapSize()
     -- Implement code for changing the size of the world map here
 end
-
-function MyAddon:AutoLooter()
+ function GnomesLazyTools:AutoLooter()
     -- Implement code for auto-looting lootable items here
 end
-
--- Register Addon Events
-MyAddon:RegisterEvent("PLAYER_LOGIN", "OnInitialize")
-MyAddon:RegisterEvent("PLAYER_ENTERING_WORLD", "OnEnable")
-MyAddon:RegisterEvent("PLAYER_LEAVING_WORLD", "OnDisable")
-
--- Register Slash Commands
+ -- Register Addon Events
+GnomesLazyTools:RegisterEvent("PLAYER_LOGIN", "OnInitialize")
+GnomesLazyTools:RegisterEvent("PLAYER_ENTERING_WORLD", "OnEnable")
+GnomesLazyTools:RegisterEvent("PLAYER_LEAVING_WORLD", "OnDisable")
+ -- Register Slash Commands
 SLASH_GNOMESTOOLS1 = "/gnomestools"
 SLASH_GNOMESTOOLS2 = "/gt"
 SlashCmdList["GNOMESTOOLS"] = function(msg)
