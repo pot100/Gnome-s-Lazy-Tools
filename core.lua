043ca3d8ae1386a375a10864d0ff97431364cf10
pot100 <126993705+pot100@users.noreply.github.com>
2023-07-08 01:38:29 -0400

    Add files via upload

---
+++ core.lua
@@ -0,0 +1,47 @@
+-- Addon Initialization
+local addonName, addonTable = ...
+local AceAddon = LibStub("AceAddon-3.0")
+local MyAddon = AceAddon:NewAddon(addonTable, addonName, "AceEvent-3.0")
+
+-- Addon Variables
+local version = "<1.0.0>"
+
+-- Addon Initialization Function
+function MyAddon:OnInitialize()
+    -- Perform initialization tasks here
+end
+
+-- Addon Enable Function
+function MyAddon:OnEnable()
+    -- Register events and set up functionality here
+end
+
+-- Addon Disable Function
+function MyAddon:OnDisable()
+    -- Clean up and unregister events here
+end
+
+-- Addon Functionality
+function MyAddon:MaxCameraZoom()
+    -- Implement code for controlling maximum camera zoom here
+end
+
+function MyAddon:ChangeWorldMapSize()
+    -- Implement code for changing the size of the world map here
+end
+
+function MyAddon:AutoLooter()
+    -- Implement code for auto-looting lootable items here
+end
+
+-- Register Addon Events
+MyAddon:RegisterEvent("PLAYER_LOGIN", "OnInitialize")
+MyAddon:RegisterEvent("PLAYER_ENTERING_WORLD", "OnEnable")
+MyAddon:RegisterEvent("PLAYER_LEAVING_WORLD", "OnDisable")
+
+-- Register Slash Commands
+SLASH_GNOMESTOOLS1 = "/gnomestools"
+SLASH_GNOMESTOOLS2 = "/gt"
+SlashCmdList["GNOMESTOOLS"] = function(msg)
+    -- Implement code for handling slash commands here
+end
